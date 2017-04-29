1326d063f9a843018b7bb6ccf58b9a8adce06717
RobertKohut <robertkohut@users.noreply.github.com>
2017-04-29 22:47:54 +0000

    Cleanup ENV variables

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  ROOT_URL: "http://138.68.234.244:8080"
 }
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    
+    <div class="fd-grid mdl-grid">
+    </div>
   </div>
 </template>
 
