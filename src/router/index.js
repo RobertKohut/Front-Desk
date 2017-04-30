5430479406a61f5bb461b813ab8fef5072843d5e
RobertKohut <robertkohut@users.noreply.github.com>
2017-04-30 21:53:08 +0000

    Display login if any response from the API returns 401

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Resource from 'vue-resource'
+import Auth from '@/assets/js/auth'
 import Index from '@/components/Index'
 import Login from '@/components/views/Login'
 import Dashboard from '@/components/views/Dashboard'
@@ -12,7 +13,20 @@ Vue.use(Resource)
 if (localStorage.getItem('id_token')) {
   Vue.http.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token')
 }
+
+// If left unspecified, send all request to the ENV API_URL.
 Vue.http.options.root = process.env.API_URL
+
+// TODO: Test 401 response
+// Present login screen if response from API is 401 (unauthorized)
+Vue.http.interceptors.push(function (request, next) {
+  // Ignore the request, we're more interested in the response
+  next(function (response) {
+    if (response.status === 401) {
+      Auth.logout()
+    }
+  })
+})
 
 export default new Router({
   mode: 'history',
