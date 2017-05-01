5d2bf11cfe5e61a04c5d1bc746f4dace5add741e
RobertKohut <robertkohut@users.noreply.github.com>
2017-05-01 05:22:36 +0000

    Prepare  static files for production build and deployment

--- config/index.js
+++ config/index.js
@@ -13,8 +13,8 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
+    productionGzip: true,
+    productionGzipExtensions: ['js', 'css', 'html'],
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
@@ -33,6 +33,6 @@ module.exports = {
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: true
   }
 }
